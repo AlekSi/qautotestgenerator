c37da6ca611a1ee6ca343475116ca18c3b797426
Benjamin C Meyer <ben@meyerhome.net>
2007-12-22 03:27:59 +0100

    Qt knows about the type 'QPixmap'

--- src/main.cpp
+++ src/main.cpp
@@ -284,7 +284,7 @@ void outputFile(ClassModelItem clazz, FunctionList functions,
         }
 
         QStringList knownTypes;
-        knownTypes << "int" << "qreal" << "bool" << "QString" << "QRectF" << "QPointF";
+        knownTypes << "int" << "qreal" << "bool" << "QString" << "QRectF" << "QPointF" << "QPixmap";
         bool knowAllTypes = true;
         for (int i = 0; i < fetchMeType.count(); ++i) {
             if (!knownTypes.contains(fetchMeType[i])) {
